4a3745327f61c4c0b83124579a4718b8b11b1d0e
Ward W. Vuillemot <wwv@mac.com>
2014-01-04 19:11:55 -0800

    Game
    - basic Sprite class
    - basic Player class
    - basic key press event draw trigger

--- controllers/game/views/includes/game.js
+++ controllers/game/views/includes/game.js
@@ -1,3 +1,82 @@
+var player;
+var gameConfiguration = {
+  keyUp: 87,
+  keyDown: 83,
+  keyLeft: 65,
+  keyRight: 68
+};
+
+$(document).keydown(function(e){
+  if (e.keyCode == gameConfiguration.keyUp) { 
+    player.moveUp();
+  }
+  if (e.keyCode == gameConfiguration.keyDown) { 
+    player.moveDown();
+  }
+  if (e.keyCode == gameConfiguration.keyLeft) { 
+    player.moveLeft();
+  }
+  if (e.keyCode == gameConfiguration.keyRight) { 
+    player.moveRight();
+  }
+});
+
+var Position = Class.extend({
+  init: function(positionX, positionY, positionZ){
+    this.x = positionX;
+    this.y = positionY;
+    this.z = positionZ;
+  }
+});
+
+var Sprite = Class.extend({
+  init: function(canvasContext, position, width, height, fillStyle, spriteImage){
+    this.canvasContext = canvasContext;
+    this.position = position;
+    this.spriteImage = spriteImage;
+    this.fillStyle = fillStyle;
+    this.height = height;
+    this.width = width;
+  },
+  draw: function(){
+    this.canvasContext.fillStyle=this.fillStyle;
+    this.canvasContext.fillRect(this.position.x, this.position.y, this.width, this.height);
+  },
+  moveUp: function(){
+    this.position.y++;
+    this.draw();
+  },
+  moveDown: function(){
+    this.position.y--;
+    this.draw();
+  },
+  moveLeft: function(){
+    this.position.x--;
+    this.draw();
+  },
+  moveRight: function(){
+    this.position.x++;
+    this.draw();
+  },
+});
+
+var Player = Sprite.extend({
+  init: function(canvasContext, positionX, positionY){
+    position = new Position(positionX, positionY, 0);
+    width = 15;
+    height = 15;
+    fillStyle = '#FF0000';
+    spriteImage = null;
+    this._super( canvasContext, position, width, height, fillStyle, spriteImage );
+  },
+  draw: function(){
+    // Call the inherited version of dance()
+    return this._super();
+  },
+});
+
+
+
 $(document).ready( function(){
   var game = $('.game');
   if(game){
@@ -8,11 +87,7 @@ $(document).ready( function(){
 
     var context = canvas.getContext("2d");
     
-    context.fillStyle="#FF0000";
-    positionX = 10;
-    positionY = 10;
-    width = 10;
-    height = 10;
-    context.fillRect(positionX, positionY, width, height);  
+    player = new Player(context, 200, 200 );
+    player.draw();
   }
 });
