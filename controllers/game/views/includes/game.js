5dfc1f242c59c1f9e05649cc456619b70a413b7c
Ward W. Vuillemot <wwv@mac.com>
2014-01-04 18:32:13 -0800

    Game
    - add square to canvas

--- controllers/game/views/includes/game.js
+++ controllers/game/views/includes/game.js
@@ -1,1 +1,18 @@
-$('.game');
+$(document).ready( function(){
+  var game = $('.game');
+  if(game){
+    var canvas = game[0];
+
+    canvas.setAttribute('width', '400px');
+    canvas.setAttribute('height', '400px');
+
+    var context = canvas.getContext("2d");
+    
+    context.fillStyle="#FF0000";
+    positionX = 10;
+    positionY = 10;
+    width = 10;
+    height = 10;
+    context.fillRect(positionX, positionY, width, height);  
+  }
+});
